104976bea48924983648e015f1f237be72f43932
Pavel_grasmik <forexgr@mail.ru>
2022-03-27 17:40:40 +0600

    finish lesson1.py and change webscraping.py

--- webscraping.py
+++ webscraping.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 # 1st Example
 
@@ -31,11 +32,43 @@ from bs4 import BeautifulSoup
 #     print(f'{n}:  {itemPrice} за {itemName}')
 
 #3 Example
-url = 'https://www.lamoda.kz/c/831/default-sports-women/?zbs_content=js_w_icons_869374_kz_2502_w_icons'
+url = 'https://question-it.com/questions/1944385/kodirovka-pycharm-55-utf-8?'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
-items = soup.find_all('div', class_='x-product-card-description__brand-name')
 
-for n, i in enumerate(items, start=1):
-    itemName = i.find('div', class_='x-product-card-description__brand-name')
-    print(f'{n}: {itemName}')
+# Метод title получает титул страницы <title>
+# title = soup.title
+
+# print(title)
+# print(title.text)
+# print(title.string)
+
+# Методы .find() .find_all() поиска по тегу
+# page_h1 = soup.find("a")
+# print(page_h1)
+# page_all_h1 = soup.find_all("a")
+# #print(page_all_h1)
+# for item in page_all_h1:
+#     print(item.text)
+# # Можно конкретизировать данный поиск указав кроме тега так же и класс
+# autor = soup.find_all("span", class_ = "author")
+#
+# print(autor)
+#
+# for item in autor:
+#     print(item.text)
+# Передача словоря в атрибуты фильтрации поиска где с помощью пары ключь - значение указываем параметры отбора
+# find_all_spans_in_misc = soup.find(class_="misc").find_all("span")
+# print(find_all_spans_in_misc[1].text)
+# for item in find_all_spans_in_misc:
+#     print(item.text)
+
+find_a_by_text = soup.find("div", text=re.compile("ответ"))
+print(find_a_by_text)
+
+find_by_text_all = soup.find_all("a", text=re.compile(("[Кк]одировку")))
+for text in find_by_text_all:
+    print(text.text)
+
+
+
